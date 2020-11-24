8a9b8a49dbac9b1175134f725beef21da4b4e9ee
Aniksaha-ui <sahaanik1045@gmail.com>
2020-11-23 20:11:21 -0800

    first commit

---
+++ app/src/main/java/com/demo/roomdemo/db/RoomAppDb.kt
@@ -0,0 +1,39 @@
+package com.demo.roomdemo.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
+
+
+@Database(entities = [UserEntity::class], version = 2)
+abstract class RoomAppDb: RoomDatabase() {
+
+
+    abstract fun userDao(): UserDao?
+
+    companion object {
+        private var INSTANCE: RoomAppDb?= null
+
+
+        fun getAppDatabase(context: Context): RoomAppDb? {
+
+            if(INSTANCE == null ) {
+
+                INSTANCE = Room.databaseBuilder<RoomAppDb>(
+                    context.applicationContext, RoomAppDb::class.java, "AppDBB"
+                )
+
+                    .allowMainThreadQueries()
+                    .build()
+
+            }
+            return INSTANCE
+        }
+        fun destroyInstance() {
+            INSTANCE = null
+        }
+    }
+}
